0428a3212aa9cb44b126a94156280cbe75229bd6
yankiwi <41830740+yankiwi@users.noreply.github.com>
2021-07-08 10:01:37 +1200

    Create collectors.go

---
+++ collectors.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+  "github.com/yankiwi/aruba_exporter/collector"
+	"github.com/yankiwi/aruba_exporter/config"
+	"github.com/yankiwi/aruba_exporter/connector"
+)
+
+type collectors struct {
+	collectors map[string]collector.RPCCollector
+	devices    map[string][]collector.RPCCollector
+	cfg        *config.Config
+}
+
+func collectorsForDevices(devices []*connector.Device, cfg *config.Config) *collectors {
+	c := &collectors{
+		collectors: make(map[string]collector.RPCCollector),
+		devices:    make(map[string][]collector.RPCCollector),
+		cfg:        cfg,
+	}
+
+	for _, d := range devices {
+		c.initCollectorsForDevice(d)
+	}
+
+	return c
+}
+
+func (c *collectors) initCollectorsForDevice(device *connector.Device) {
+	f := c.cfg.FeaturesForDevice(device.Host)
+
+	c.devices[device.Host] = make([]collector.RPCCollector, 0)
+
+}
+
+func (c *collectors) addCollectorIfEnabledForDevice(device *connector.Device, key string, enabled *bool, newCollector func() collector.RPCCollector) {
+	if !*enabled {
+		return
+	}
+
+	col, found := c.collectors[key]
+	if !found {
+		col = newCollector()
+		c.collectors[key] = col
+	}
+
+	c.devices[device.Host] = append(c.devices[device.Host], col)
+}
+
+func (c *collectors) allEnabledCollectors() []collector.RPCCollector {
+	collectors := make([]collector.RPCCollector, len(c.collectors))
+
+	i := 0
+	for _, collector := range c.collectors {
+		collectors[i] = collector
+		i++
+	}
+
+	return collectors
+}
+
+func (c *collectors) collectorsForDevice(device *connector.Device) []collector.RPCCollector {
+	cols, found := c.devices[device.Host]
+	if !found {
+		return []collector.RPCCollector{}
+	}
+
+	return cols
+}
