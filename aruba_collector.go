dd362c9d759ee693130897b674d462e3bd17f0c2
yankiwi <41830740+yankiwi@users.noreply.github.com>
2021-07-08 22:43:06 +1200

    Update aruba_collector.go

--- aruba_collector.go
+++ aruba_collector.go
@@ -89,6 +89,7 @@ func (c *arubaCollector) collectForHost(device *connector.Device, ch chan<- prom
 
 	for _, col := range c.collectors.collectorsForDevice(device) {
 		ct := time.Now()
+		log.Infof("collector: %s", col)
 		err := col.Collect(client, ch, l)
 
 		if err != nil && err.Error() != "EOF" {
