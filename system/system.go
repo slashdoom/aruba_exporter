892727dad0434a877a6bcf459d75f469dddb879a
yankiwi <41830740+yankiwi@users.noreply.github.com>
2021-07-08 14:10:43 +1200

    Update system.go

--- system/system.go
+++ system/system.go
@@ -1,17 +1,17 @@
 package system
 
-type VersionFact struct {
+type SystemVersion struct {
 	Version string
 }
 
-type MemoryFact struct {
+type SystemMemory struct {
 	Type  string
 	Total float64
 	Used  float64
 	Free  float64
 }
 
-type CPUFact struct {
+type SystemCPU struct {
 	FiveSeconds float64
 	Interrupts  float64
 	OneMinute   float64
