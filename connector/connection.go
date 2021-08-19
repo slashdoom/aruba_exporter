c5c902c593be499989d6f3548f5f0b75b4abb78e
T823364 <T823364@SPKX1030001.local>
2021-08-19 15:12:22 +1200

    Update connection.go

--- connector/connection.go
+++ connector/connection.go
@@ -163,6 +163,7 @@ func (c *SSHConnection) readln(ch chan result, cmd string, r io.Reader) {
 		if err != nil {
 			ch <- result{output: "", err: err}
 		}
+		log.Debugln(string(buf[:n]))
 		loadStr += string(buf[:n])
 		if strings.Contains(loadStr, cmd) && re.MatchString(loadStr) {
 			break
