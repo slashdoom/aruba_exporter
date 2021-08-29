7963a129a1896919ef84ee99aacf5e09f6ce9db8
T823364 <T823364@SPKX1030001.local>
2021-08-30 08:09:09 +1200

    Update aruba_collector.go

--- aruba_collector.go
+++ aruba_collector.go
@@ -80,7 +80,7 @@ func (c *arubaCollector) collectForHost(device *connector.Device, ch chan<- prom
 
 	ch <- prometheus.MustNewConstMetric(upDesc, prometheus.GaugeValue, 1, l...)
 
-	client := rpc.NewClient(conn, cfg.Debug)
+	client := rpc.NewClient(conn, cfg.Level)
 	err = client.Identify()
 	if err != nil {
 		log.Errorln(device.Host + ": " + err.Error())
