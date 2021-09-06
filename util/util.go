9679c1710ebe2feb4c57ee32689a4a0c702dd03b
T823364 <T823364@SPKX1030001.local>
2021-09-06 21:24:25 +1200

    Update util.go

--- util/util.go
+++ util/util.go
@@ -8,26 +8,26 @@ import (
 )
 
 // Str2float64 converts a string to float64
-func Str2float64(str string) float64 {
-	str.ReplaceAllString(",", "")
-	value, err := strconv.ParseFloat(str, 64)
+func Str2float64(s string) float64 {
+	ns := strings.Replace(s, ",", "", -1)
+	value, err := strconv.ParseFloat(ns, 64)
 	if err != nil {
 		return -1
 	}
 	return value
 }
 
-func StandardizeMacAddr(str string) string {
-	macRegex := regexp.MustCompile(`([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})`)
-	if matches := macRegex.FindStringSubmatch(str); matches != nil {
-		mac := fmt.Sprintf("%s-%s-%s-%s-%s-%s", 
+func StandardizeMacAddr(s string) string {
+	r := regexp.MustCompile(`([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})[\.\-:]?([0-9A-Fa-f]{2})`)
+	if matches := r.FindStringSubmatch(s); matches != nil {
+		m := fmt.Sprintf("%s-%s-%s-%s-%s-%s", 
 			matches[1],
 			matches[2],
 			matches[3],
 			matches[4],
 			matches[5],
 			matches[6]) 
-		return strings.ToUpper(mac)
+		return strings.ToUpper(m)
 	}
 	return ""
 }
