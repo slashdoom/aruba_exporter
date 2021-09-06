2ccacae07e42467f464dcc49c670af7765314be8
T823364 <T823364@SPKX1030001.local>
2021-09-06 21:19:26 +1200

    Update util.go

--- util/util.go
+++ util/util.go
@@ -9,6 +9,7 @@ import (
 
 // Str2float64 converts a string to float64
 func Str2float64(str string) float64 {
+	str.ReplaceAllString(",", "")
 	value, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		return -1
