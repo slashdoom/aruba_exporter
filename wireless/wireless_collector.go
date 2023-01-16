e9378a2811be74365530271df76517b743139381
paddy <5092581+slashdoom@users.noreply.github.com>
2023-01-16 15:56:09 +1300

    wireless dev

--- wireless/wireless_collector.go
+++ wireless/wireless_collector.go
@@ -1,11 +1,3 @@
 package wireless
 
-import (
-	"github.com/slashdoom/aruba_exporter/collector"
-	"github.com/slashdoom/aruba_exporter/rpc"
-
-	"github.com/prometheus/client_golang/prometheus"
-	log "github.com/sirupsen/logrus"
-)
-
 const prefix string = "aruba_system_"
